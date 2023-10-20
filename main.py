b071679f8f26c33e2156fb36ce1ef3942403b44e
Danila-Bogovik <Danila.Bogovik@gmail.com>
2023-10-20 12:35:48 +0300

    commit3

--- main.py
+++ main.py
@@ -11,10 +11,6 @@ db = mysql.connector.connect(
     database="yourdatabase"
 )
 
-# Создание таблицы в базе данных (если она еще не создана)
-cursor = db.cursor()
-cursor.execute("CREATE TABLE IF NOT EXISTS files (id INT AUTO_INCREMENT PRIMARY KEY, filename VARCHAR(255), filedata LONGBLOB)")
-
 @app.route("/", methods=["GET", "POST"])
 def upload_file():
     if request.method == "POST":
